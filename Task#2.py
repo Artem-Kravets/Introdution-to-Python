169d09b4083896e520cbabf562ae77bf78d2a630
Artem Kravets <51452093+Artem-Kravets@users.noreply.github.com>
2021-01-08 20:17:34 +0200

    Add the Homework №5(decorators)

---
+++ Task#2.py
@@ -0,0 +1,27 @@
+import time
+import logging
+
+logging.basicConfig(filename="my_log.log", level=logging.INFO)
+
+
+def log_decorator(func):
+
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        run_time = end_time - start_time
+        logging.info(
+            f"Function Name - {func.__name__} with Args - {args} and Kwargs - {kwargs} run in {run_time}"
+        )
+        return result
+    return wrapper_timer
+
+
+@log_decorator
+def wishes(*args, **kwargs):
+    time.sleep(0.3)
+    print(args, kwargs)
+
+
+wishes(["Happy", "New"], year=2021, this_year="cow")
