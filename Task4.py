ad053b9d3c35599772404351a8b214b5b2c84ebb
Artem Kravets <51452093+Artem-Kravets@users.noreply.github.com>
2021-01-15 20:05:17 +0200

    Update Task#4

--- Task4.py
+++ Task4.py
@@ -1,14 +1,13 @@
 def my_generator(input_string, max_times, step=1):
-    words = input_string.split(" ")
     index = 0
     inner_index = 0
     while inner_index < max_times:
-        yield words[index]
-        index = (index + 1) % len(words)
+        yield input_string[index]
+        index = (index + 1) % len(input_string)
         inner_index += step
 
 
-my_gen = my_generator("Winter Spring Summer Autumn", 9)
+my_gen = my_generator(["Winter", "Spring", "Summer", "Autumn"], 9)
 
 for i in my_gen:
     print(i)
