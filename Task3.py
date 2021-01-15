702a01806acd73681066620901b74097802d9e71
Artem Kravets <51452093+Artem-Kravets@users.noreply.github.com>
2021-01-15 19:36:48 +0200

    Add Task#3

---
+++ Task3.py
@@ -0,0 +1,31 @@
+class Circle:
+
+    def __init__(self, seasons, max_range):
+        self.seasons = seasons.split(" ")
+        self.max_range = max_range
+
+    def __iter__(self):
+        return CircleIterator(self)
+
+
+class CircleIterator:
+
+    def __init__(self, circle):
+        self.my_circle = circle
+        self.inner_index = 0
+        self.index = 0
+
+    def __next__(self):
+        if self.inner_index >= self.my_circle.max_range:
+            raise StopIteration
+        output = self.my_circle.seasons[self.index]
+        self.index = (self.index + 1) % len(self.my_circle.seasons)
+        self.inner_index += 1
+        return output
+
+
+my_circle = Circle("Hello this beautiful world", 7)
+
+
+for i in my_circle:
+    print(i)
